90e8ab9c25f7eb14cc79f12b70b0c62c10d69abf
Sylvie Dagoret-Campagne <dagoret@lal.in2p3.fr>
2016-11-25 22:38:06 +0100

    correct for bin size

--- inst1_2d_asm_Nx_M2I1NXASM/libGMAOMERRA2Data.py
+++ inst1_2d_asm_Nx_M2I1NXASM/libGMAOMERRA2Data.py
@@ -175,7 +175,7 @@ def AreaSelect(X,Y,data,LongMin,LongMax,LatMin,LatMax):
 #---------------------------------------------------------------------------------    
     
 #---------------------------------------------------------------------------------    
-def SelectBin(X,Y,data,Long0,Lat0,DLong=1.0,DLat=1.0):
+def SelectBin(X,Y,data,Long0,Lat0,DLong=0.625,DLat=0.498614958449):
     '''
     SelectBin(X,Y,data,Long0,Lat0,DLong=1.0,DLat=1.0)
     =================================================
